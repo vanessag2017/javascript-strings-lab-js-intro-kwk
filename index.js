0f9b882de9b65cbb2d4282c5e9ee0363be10a833
vanessag217 <vanessagarcia61399@gmail.com>
2016-11-01 17:51:12 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,17 @@
+var greeting = "Awww yea";
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = "" + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `${topic}`;
+
+var writeGreetingToDocument = `something to do with document...`;
+
+// Type your answer into the console without the quote marks and see if it works.
+
+var customCelebration = 'Kaboomie! Hachewie, yeaaaaaaaah. Shwing.';
+
+var consoleCelebration = `something to do with console...`;
